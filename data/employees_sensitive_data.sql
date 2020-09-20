cf9d050f6082858291e2a43d0a31c08acf6913fa
Jun Wei <kjunwei1996@gmail.com>
2020-09-20 22:23:00 +0800

    Update schema

---
+++ data/employees_sensitive_data.sql
@@ -0,0 +1,24 @@
+DELETE FROM employees_sensitive_data;
+
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (1,'105-5697 Eget Road','095-556-8612',8846,'fringilla mi lacinia mattis. Integer eu lacus. Quisque imperdiet, erat');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (2,'Ap #791-1187 Fusce St.','091-593-3407',8676,'nunc id enim. Curabitur massa. Vestibulum accumsan neque et nunc.');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (3,'308-2602 Elementum Avenue','023-270-2423',6111,'Maecenas iaculis aliquet diam. Sed diam lorem, auctor quis, tristique');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (4,'Ap #652-1532 Et Rd.','061-316-8492',7876,'nisi. Aenean eget metus. In nec orci. Donec nibh. Quisque');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (5,'931-7351 Commodo Ave','065-009-4951',7969,'dolor sit amet, consectetuer adipiscing elit. Etiam laoreet, libero et');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (6,'909-1717 Nibh St.','014-907-8372',8444,'neque. Sed eget lacus. Mauris non dui nec urna suscipit');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (7,'P.O. Box 263, 1353 Iaculis Rd.','002-465-0944',7804,'nec, diam. Duis mi enim, condimentum eget, volutpat ornare, facilisis');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (8,'P.O. Box 595, 2479 Ultrices, Road','047-585-3984',8037,'aliquet molestie tellus. Aenean egestas hendrerit neque. In ornare sagittis');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (9,'8996 Non, Ave','011-276-8405',6240,'at pede. Cras vulputate velit eu sem. Pellentesque ut ipsum');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (10,'893-3130 Cursus Rd.','021-380-6116',6765,'Cras dolor dolor, tempus non, lacinia at, iaculis quis, pede.');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (11,'P.O. Box 520, 8150 Vestibulum. Ave','058-515-6213',8471,'est, congue a, aliquet vel, vulputate eu, odio. Phasellus at');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (12,'2821 Sed Street','096-408-9918',5838,'a, aliquet vel, vulputate eu, odio. Phasellus at augue id');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (13,'5508 Id, Rd.','006-482-4477',7459,'risus a ultricies adipiscing, enim mi tempor lorem, eget mollis');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (14,'710-8334 Non, Street','068-031-6493',5589,'at pede. Cras vulputate velit eu sem. Pellentesque ut ipsum');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (15,'Ap #551-8469 Ultricies Avenue','049-827-1246',5143,'arcu. Vivamus sit amet risus. Donec egestas. Aliquam nec enim.');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (16,'P.O. Box 241, 8195 Sapien, Rd.','016-916-2983',5263,'dictum eleifend, nunc risus varius orci, in consequat enim diam');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (17,'Ap #385-8907 Neque Rd.','002-075-7649',6813,'quam quis diam. Pellentesque habitant morbi tristique senectus et netus');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (18,'P.O. Box 864, 2548 Leo. Ave','053-804-8536',8622,'sed, est. Nunc laoreet lectus quis massa. Mauris vestibulum, neque');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (19,'8542 Nunc Ave','060-040-0602',5919,'Integer tincidunt aliquam arcu. Aliquam ultrices iaculis odio. Nam interdum');
+INSERT INTO employees_sensitive_data (id,address,phone,salary,biography) VALUES (20,'Ap #913-2650 Luctus Ave','094-292-9182',8742,'rhoncus. Proin nisl sem, consequat nec, mollis vitae, posuere at,');
+
+SELECT * FROM employees_sensitive_data;
