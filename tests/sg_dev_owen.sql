1b68894fdfcd014840bbd53e619089dc132d09bd
Tonyh5chen <ch19870525@gmail.com>
2020-09-20 23:13:12 +0800

    update test case

--- tests/sg_dev_owen.sql
+++ tests/sg_dev_owen.sql
@@ -2,12 +2,28 @@
 -- OWEN is a manager in Software Development stationed in Singapore.
 -- Software Development belongs to a normal group type in the HR system.
 SET ROLE NON_SYSTEM, MANAGER;
-
+SET SERVEROUTPUT ON;
+-- Expected: Should only return OWEN's payslip.
+DECLARE
+    counter INT;
+BEGIN
+    SELECT COUNT(*)
+        INTO counter
+        FROM SYSTEM.payslips P
+        INNER JOIN SYSTEM.employees E 
+        ON P.recipient = E.id;
+    IF counter != 1 THEN
+        RAISE_APPLICATION_ERROR(-20000, 'Incorrect number of employees.');
+        ELSE 
+        DBMS_OUTPUT.PUT_LINE('READ Test Passed');
+    END IF;
+END;
+/
 -- Expected: OWEN Should not be able to update/delete its own payslip.
 DECLARE
     counter INT;
 BEGIN
-    DBMS_OUTPUT.PUT_LINE('START');
+    --DBMS_OUTPUT.PUT_LINE('START');
     UPDATE SYSTEM.payslips SET 
         amount_paid = 100000
         WHERE id = 25;
--- tests/sg_hr_xerxes.sql
+++ tests/sg_hr_xerxes.sql
@@ -1,14 +1,18 @@
 -- This test file is for the user XERXES with password of XERXES.
 -- XERXES is a HR stationed in Singapore.
 SET ROLE NON_SYSTEM, HR;
-
--- Expected: Should only return the payslip from all the employees in Singapore.
+SET SERVEROUTPUT ON;
+-- Expected: Should only return all local payslips.
 DECLARE
     counter INT;
 BEGIN
-    SELECT COUNT(*) INTO counter FROM SYSTEM.payslips;
+    SELECT COUNT(*) 
+    INTO counter FROM SYSTEM.payslips;
     IF counter != 58 THEN
         RAISE_APPLICATION_ERROR(-20000, 'Incorrect number of employees.');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('local employee=' || counter);
+        DBMS_OUTPUT.PUT_LINE('READ Test Passed');
     END IF;
 END;
 /
@@ -22,12 +26,16 @@ BEGIN
     counter := SQL%rowcount;
     IF counter != 1 THEN
         RAISE_APPLICATION_ERROR(-20000, 'Should update 1 row');
+        ELSE
+        DBMS_OUTPUT.PUT_LINE('ABLE TO UPDTAE LOCAL Test Passed');
     END IF;
     DELETE SYSTEM.payslips 
         WHERE id = 25;
     counter := SQL%rowcount;
     IF counter != 1 THEN
         RAISE_APPLICATION_ERROR(-20000, 'Should delete 1 row.');
+        ELSE
+         DBMS_OUTPUT.PUT_LINE('ABLE TO DELETE LOCAL Test Passed');
     END IF;
     ROLLBACK;
 END;
@@ -36,20 +44,22 @@ END;
 DECLARE
     counter INT;
 BEGIN
-    UPDATE SYSTEM.claims SET 
-        amount = 10000
+    UPDATE SYSTEM.payslips SET 
+        amount_paid = 10000
         WHERE id = 80;
     counter := SQL%rowcount;
     IF counter != null THEN
         RAISE_APPLICATION_ERROR(-20000, 'Should update 0 row');
+        ELSE
+        DBMS_OUTPUT.PUT_LINE('UNABLE TO UPDTAE OTHER Test Passed');
     END IF;
-    DELETE SYSTEM.claims 
+    DELETE SYSTEM.payslips 
     WHERE id = 80;
     counter := SQL%rowcount;
     IF counter != null THEN
         RAISE_APPLICATION_ERROR(-20000, 'Should delete 0 row');
+        ELSE
+        DBMS_OUTPUT.PUT_LINE('UNABLE TO UPDTAE OTHER Test Passed');
     END IF;
     ROLLBACK;
 END;
-/
-ROLLBACK;
